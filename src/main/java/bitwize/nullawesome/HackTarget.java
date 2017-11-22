df663158a0fc3ba1d97152ede22adf721d43470f
Jeff Read <bitwize@gmail.com>
2017-11-22 10:54:16 -0500

    forgot HackTarget class

---
+++ src/main/java/bitwize/nullawesome/HackTarget.java
@@ -0,0 +1,12 @@
+package bitwize.nullawesome;
+
+import android.graphics.RectF;
+import android.graphics.PointF;
+
+public class HackTarget {
+    float width;
+    float height;
+    int requiresKeyMask;
+    byte hasKey;
+    boolean hacked;
+}
